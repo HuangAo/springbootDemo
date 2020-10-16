168439130b648717fcce097ea88b6bb6b9e2f672
huangao <huangao23@163.com>
2020-10-16 10:06:23 +0800

    登录页面国际化

--- springboot-02-web/src/main/java/com/huang/config/MyMVCConfig.java
+++ springboot-02-web/src/main/java/com/huang/config/MyMVCConfig.java
@@ -1,6 +1,9 @@
 package com.huang.config;
 
+import org.springframework.boot.autoconfigure.web.servlet.WebMvcProperties;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.LocaleResolver;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -15,5 +18,10 @@ public class MyMVCConfig implements WebMvcConfigurer {
         //配置首页视图控制
         registry.addViewController("/").setViewName("index");
         registry.addViewController("/index.html").setViewName("index");
+    }
+
+    @Bean
+    public LocaleResolver localeResolver(){
+        return new MyLocaleResolver();
     }
 }

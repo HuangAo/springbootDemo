82ccba3b32a7156d298e67a1e03a291c8d67e375
huangao <huangao23@163.com>
2020-10-16 15:01:29 +0800

    配置Druid数据源 后台监控

---
+++ springboot-02-web/src/main/java/com/huang/config/DruidConfig.java
@@ -0,0 +1,38 @@
+package com.huang.config;
+
+import com.alibaba.druid.pool.DruidDataSource;
+import com.alibaba.druid.support.http.StatViewServlet;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.sql.DataSource;
+
+/**
+ * @author :huangao
+ */
+@Configuration
+public class DruidConfig {
+
+    @Bean
+    @ConfigurationProperties(prefix = "spring.datasource")
+    public DataSource druidDataSource() {
+        return new DruidDataSource();
+    }
+
+    //后台监控
+    @Bean
+    public ServletRegistrationBean druidServlet() {
+        // 现在要进行druid监控的配置处理操作
+        ServletRegistrationBean servletRegistrationBean = new ServletRegistrationBean(
+                new StatViewServlet(), "/druid/*");
+
+        // 控制台管理用户名
+        servletRegistrationBean.addInitParameter("loginUsername", "admin");
+        // 控制台管理密码
+        servletRegistrationBean.addInitParameter("loginPassword", "admin");
+
+        return servletRegistrationBean ;
+    }
+}
